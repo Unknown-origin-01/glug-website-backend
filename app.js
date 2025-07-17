66a30ef50b35e5ee4090459c5c5466c4d89706e1
Priyam-0001 <priyamnath5@gmail.com>
2025-07-17 22:56:21 +0530

    Starting

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require('express')
+const mongoose = require('mongoose')
+
+const userRoutes = require("./routes/user")
+const adminRoutes = require("./routes/admin")
+
+const app = express()
+app.use(express.json())
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err)=>{console.log(err)})
+
+// app.use("/api/user", userRoutes)
+// app.use("/api/admin", adminRoutes)
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server started at PORT: ${process.env.PORT}`)
+})
